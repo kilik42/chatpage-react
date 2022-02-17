d1d2933bfa7f838702ad89a8d99816836b2d024b
kilik42 <marvinevins@yahoo.com>
2022-02-17 12:43:17 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import {
+  BrowswerRouer as Router, Routes, Route
+} from "react-router-dom";
+import Home from './components/Home';
+import ChatPage from './components/ChatPage';
+
+function App() {
+  return (
+    <Router>
+        <div className="App">
+          <Routes>
+            <Route path='/chatpage' element={<ChatPage/>}/>
+            <Route path='/' element={<Home/>}/>
+          </Routes>
+      </div>
+      
+    </Router>
+    
+  );
+}
+
+export default App;
